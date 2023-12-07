a2566e45d14d8ea0dd2b8919cd34af3e8ebb31ae
Choi_Dongeun <Dongeun.Choi0128@gmail.com>
2023-12-08 01:28:26 +0900

    Tetris commit

--- anotherTETRIS/Tetris/Tetris/Program.cs
+++ anotherTETRIS/Tetris/Tetris/Program.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TETRIS;
 
-namespace TETRIS
+namespace Tetris
 {
     internal static class Program
     {
-        
-        
+        /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
-       
-        
+        /// </summary>
         [STAThread]
         static void Main()
         {

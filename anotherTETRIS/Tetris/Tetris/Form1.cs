58546127c002fe2d3b3afc9c0ab451735d9c7917
Choi_Dongeun <Dongeun.Choi0128@gmail.com>
2023-12-08 01:42:07 +0900

    Form1.cs commit

--- anotherTETRIS/Tetris/Tetris/Form1.cs
+++ anotherTETRIS/Tetris/Tetris/Form1.cs
@@ -7,14 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using Tetris;
 
 namespace Tetris
 {
-    public partial class Form1 : Form
+    enum NetworkStatus
     {
-        public Form1()
+        notConnected,
+        Server,
+        Client
+    }
+    public partial class NetworkTetris : Form
+    {
+        Game myGame;
+
+        Socket socket;
+        Thread receiveThread;
+        IPAddress ipAdress;
+        int port = 9999;
+        byte[] receiveData = new byte[1024];
+        string encodingData;
+
+        private const int EDGE_SIZE_X = 2;
+        private const int EDGE_SIZE_Y = 2;
+        private int downTickInterval = 600;
+        private int lastScore = 0;
+        private int attackPoint;
+        private int playTime = 0;
+        private bool isWin = false;
+        private bool putUp = false;
+        private bool putShift = false;
+        private bool isPlay;
+        private bool[,] playerBoard = new bool[Game.BY, Game.BX];
+
+        Point boardStartPoint;
+        Point boardEndPoint;
+        Point playerBoardStartPoint;
+        Point playerBoardEndPoint;
+        Rectangle startButtonRect;
+        Rectangle quitButtonRect;
+
+        NetworkStatus networkStatus = NetworkStatus.notConnected;
+        SolidBrush[] blockBrushes = new SolidBrush[8];
+        SolidBrush backgroundBrush = new SolidBrush(Color.FromArgb(255, 30, 30, 40));
+
+        public NetworkTetris()
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(255, 70, 70, 80);
+            SetBlockBrushes();
+            SetSize();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            myGame = new Game();
+            attackPoint = 150;
+            downTickInterval = 600;
+            isPlay = false;
+        }
+        private void SetBlockBrushes()
+        {
+            blockBrushes[0] = new SolidBrush(Color.FromArgb(000, 255, 255));
+            blockBrushes[1] = new SolidBrush(Color.FromArgb(255, 255, 000));
+            blockBrushes[2] = new SolidBrush(Color.FromArgb(051, 000, 255));
+            blockBrushes[3] = new SolidBrush(Color.FromArgb(255, 100, 000));
+            blockBrushes[4] = new SolidBrush(Color.FromArgb(255, 000, 000));
+            blockBrushes[5] = new SolidBrush(Color.FromArgb(000, 200, 000));
+            blockBrushes[6] = new SolidBrush(Color.FromArgb(255, 000, 255));
+            blockBrushes[7] = new SolidBrush(Color.FromArgb(200, 200, 200));
+        }
+        private void SetSize()
+        {
+            int formSizeX = EDGE_SIZE_X * 5 + Game.BX * 2 + Game.OPTION_SIZE_X * 2;
+            int formSizeY = EDGE_SIZE_Y * 2 + Game.BY;
+            SetClientSizeCore(formSizeX * Game.CELL_SIZE, formSizeY * Game.CELL_SIZE); //클라이언트 사이즈
+
+            boardStartPoint = new Point(EDGE_SIZE_X * 2 + Game.OPTION_SIZE_X, EDGE_SIZE_Y); //보드판 사이즈
+            boardEndPoint = new Point(boardStartPoint.X + Game.BX, EDGE_SIZE_Y + Game.BY);
+            playerBoardStartPoint = new Point(EDGE_SIZE_X * 4 + Game.OPTION_SIZE_X * 2 + Game.BX, EDGE_SIZE_Y);
+            playerBoardEndPoint = new Point(playerBoardStartPoint.X + Game.BX, EDGE_SIZE_X + Game.BY);
+
+        }
